c1e2d4903e0c48fd4365dfe997b2a515d80547c7
kishorekadali21 <kishorekadali21@gmail.com>
2023-07-23 13:53:53 +0530

    first commit

---
+++ NestedP5.java
@@ -0,0 +1,14 @@
+package com.loop;
+
+public class NestedP5 {
+	public static void main(String [] args) {
+		
+		for (char i='A';i<='E';i++) {
+			for (char j='A';j<=i;j++) {
+				System.out.print(i+ " ");
+			}
+			System.out.println();
+		}
+	}
+
+}
